9becd873b15159e04b23f1884cc8df7636cb8aa3
HoangNam <>
2020-08-14 22:00:43 +0700

    fir

---
+++ Section2/Lesson/index.js
@@ -0,0 +1,14 @@
+// let x = 0;
+// while (x < 3) {
+//     console.log("toi la nam");
+//     x++;
+// }
+// for (let x = 0; x < 3; x++) {
+//        console.log(`toi la Nam`);
+// }
+let age = 27;
+if (age < 10) {
+    console.log(`toi co so tuoi la: ${age}`);
+} else if (age < 30) {
+    console.log(`toi co so tuoi la ${age}`);
+}
---
+++ Section2/Lesson/index2.js
@@ -0,0 +1,8 @@
+let age = prompt("Nhap so tuoi cua ban");
+if (age <= 10) {
+    alert(`toi nam nay ${age}`);
+} else if (age < 18) {
+    alert(`ban duoc di an kem`);
+} else {
+    alert(`minh di xem phim`);
+}
