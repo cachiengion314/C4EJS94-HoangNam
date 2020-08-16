0e01c3afdad771e51b8c234f2d647706cdeab8bc
Hoang Nam <cachiengion314@gmail.com>
2020-08-17 00:55:19 +0700

    update bai tap 13

--- Section2/Homework-13/index.js
+++ Section2/Homework-13/index.js
@@ -1,1 +1,9 @@
-// let n = parseInt(prompt(`Please enter n polygons`)); while (n > 2) { for (let i = 0; i < n; i++) { fd((1 + Math.pow(n, 2) - 50 * n) / n); rt(180 - (180 * (n - 2) / n)); } n--; }
+let number = parseInt(prompt(`Please enter number of polygons`)); 
+let n = number -2;
+while (n > 2) { 
+    for (let i = 0; i < n; i++) { 
+        fd((1 + Math.pow(n, 2) - 50 * n) / n); 
+        rt(180 - (180 * (n - 2) / n)); 
+    } 
+    n--; 
+}
