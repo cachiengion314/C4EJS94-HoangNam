f65e3c81cce44a7c6a035f4606561ce1e13d4eb0
Hoang Nam <cachiengion314@gmail.com>
2020-08-17 01:10:40 +0700

    update index.js cua homework13

--- Section2/Homework-12/index.js
+++ Section2/Homework-12/index.js
@@ -1,3 +1,7 @@
-// console.log(`Use JS Turtle to draw a polygon, the number of polygon’s edges entered by users
-// `);
-// let n = parseInt(prompt(`Please enter n number`)); for(let i= 0; i< n;i++){fd(3*100/n);rt(180-(180*(n-2)/n));}
+console.log(`Use JS Turtle to draw a polygon, the number of polygon’s edges entered by users
+`);
+let n = parseInt(prompt(`Please enter amount of edges`));
+for (let i = 0; i < n; i++) {
+    fd(3 * 100 / n);
+    rt(180 - (180 * (n - 2) / n));
+}
