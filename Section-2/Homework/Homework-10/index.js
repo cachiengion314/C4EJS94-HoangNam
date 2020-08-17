0bfb453751a91c420387c47a7ca06112d99be921
Hoang Nam <cachiengion314@gmail.com>
2020-08-17 23:48:38 +0700

    Thay doi section-2/Hwork-10/index.js

--- Section-2/Homework/Homework-10/index.js
+++ Section-2/Homework/Homework-10/index.js
@@ -6,11 +6,11 @@ let height = parseFloat(prompt(`Please, enter your height in cm`));
 let BMI = weight / Math.pow(height / 100, 2);
 if (BMI < 16) {
     alert(`you are severely underweight`);
-} else if (BMI >= 16 && BMI < 18.5) {
+} else if (BMI < 18.5) {
     alert(`you are underweight`);
-} else if (BMI > 18.5 && BMI <= 25) {
+} else if (BMI <= 25) {
     alert(`you are normal! Congratulation!`);
-} else if (BMI > 25 && BMI <= 30) {
+} else if (BMI <= 30) {
     alert(`you are overweight`);
 } else {
     alert(`you are surely obese`);
