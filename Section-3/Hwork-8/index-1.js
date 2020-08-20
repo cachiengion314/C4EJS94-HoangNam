2c18ee45f24a1f31b376b00b885dd88568a99727
HoangNam <cachiengion314@gmail.com>
2020-08-20 18:04:17 +0700

    update hwork

---
+++ Section-3/Hwork-8/index-1.js
@@ -0,0 +1,8 @@
+console.log(`8. You are a shepherd who owns a flock of sheep
+`);
+console.log(`Each of your sheep of your flock has varied sizes:
+`);
+console.log(` 8.1 Create an array to represent the sizes of your flock, 
+and log all of your flock size
+`);
+console.log(`Hello! My name is Hoang Nam. This is my sheep size`);
