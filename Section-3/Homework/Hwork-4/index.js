608e3e4e774be45d087f87246cd6f61e78e75fac
Hoang Nam company <cachiengion314@gmail.com>
2020-08-21 14:32:55 +0700

    chua bai tap

--- Section-3/Homework/Hwork-4/index.js
+++ Section-3/Homework/Hwork-4/index.js
@@ -6,33 +6,33 @@ let arrayItems = [`Jeans`, `T-Shirt`, `Socks`];
 while (true) {
     customerOrder = prompt(`Hi there, welcome to shop admin panel, what do you want(C, R, U, D)?
     `);
-    if (customerOrder == `c` || customerOrder == `d`
-        || customerOrder == `r` || customerOrder == `u`) {
-
-        if (customerOrder == `r`) {
-            console.log(`The current items are: `);
-            for (let i = 0; i < arrayItems.length; i++) {
-                console.log(arrayItems[i]);
-            }
-            break;
-        } else if (customerOrder == `c`) {
-            let newItem = prompt(`Enter the name of new item`);
-            if (newItem != undefined || newItem != null || newItem != "") {
-                arrayItems.push(newItem);
-                alert(`Done!`);
-            }
-        } else if (customerOrder == `u`) {
-            let positionUpdate = Number(prompt(`Enter the position you want to update`));
-            while (positionUpdate >= arrayItems.length) {
-                alert(`The number you pass is larger than array length -1`);
-                positionUpdate = Number(prompt(`Enter the position you want to update again`));
-            }
-            arrayItems[positionUpdate] = prompt(`Enter the new name`);
-        } else if (customerOrder == `d`) {
-            let positionDelete = prompt(`Enter the position you want to delete`);
-            arrayItems.splice(positionDelete, 1);
+    if (customerOrder == `r`) {
+        console.log(`The current items are: `);
+        for (let i = 0; i < arrayItems.length; i++) {
+            console.log(arrayItems[i]);
+        }
+        break;
+    } else if (customerOrder == `c`) {
+        let newItem = prompt(`Enter the name of new item`);
+        if (newItem != undefined || newItem != null || newItem != "") {
+            arrayItems.push(newItem);
             alert(`Done!`);
         }
+    } else if (customerOrder == `u`) {
+        let positionUpdate = Number(prompt(`Enter the position you want to update`));
+        while (positionUpdate >= arrayItems.length) {
+            alert(`The number you pass is larger than array length -1`);
+            positionUpdate = Number(prompt(`Enter the position you want to update again`));
+        }
+        arrayItems[positionUpdate] = prompt(`Enter the new name`);
+    } else if (customerOrder == `d`) {
+        let positionDelete = prompt(`Enter the position you want to delete`);
+        while (positionDelete >= arrayItems.length) {
+            alert(`The number you pass is larger than array length -1`);
+            positionDelete = Number(prompt(`Enter the position you want to delete again`));
+        }
+        arrayItems.splice(positionDelete, 1);
+        alert(`Done!`);
     } else {
         alert(`This command is not support`);
     }
