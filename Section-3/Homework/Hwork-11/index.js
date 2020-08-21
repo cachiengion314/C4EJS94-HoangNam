414bbae5191c7cb2e613737c4d4dc9a238389ba7
Hoang Nam company <cachiengion314@gmail.com>
2020-08-21 18:01:03 +0700

    Test thu bai hoc moi o Hwork 11

--- Section-3/Homework/Hwork-11/index.js
+++ Section-3/Homework/Hwork-11/index.js
@@ -1,16 +1,31 @@
-console.log(`11.
-Write a script asking users to enter a sequence of Numbers, separated by commas (,). Create a new array containing only the odd Numbers of the entered sequence
-`);
-let str = prompt(`Please enter a sequence of numbers, separated by commas`);
-let arrStrNumbers = str.split(`,`);
-let arrNumbers = arrStrNumbers.map(item => parseFloat(item));
-let strModified = `${str} => `;
-let arrOddNumbers = arrNumbers.filter(item => item % 2 != 0);
-for (let i = 0; i < arrOddNumbers.length; i++) {
-    if (i < arrOddNumbers.length - 1) {
-        strModified += arrOddNumbers[i] + `, `;
-    } else {
-        strModified += arrOddNumbers[i];
-    }
-}
-alert(strModified);
+// console.log(`11.
+// Write a script asking users to enter a sequence of Numbers, separated by commas (,). Create a new array containing only the odd Numbers of the entered sequence
+// `);
+// let str = prompt(`Please enter a sequence of numbers, separated by commas`);
+// let arrStrNumbers = str.split(`,`);
+// let arrNumbers = arrStrNumbers.map(item => parseFloat(item));
+// let strModified = `${str} => `;
+// let arrOddNumbers = arrNumbers.filter(item => item % 2 != 0);
+// for (let i = 0; i < arrOddNumbers.length; i++) {
+//     if (i < arrOddNumbers.length - 1) {
+//         strModified += arrOddNumbers[i] + `, `;
+//     } else {
+//         strModified += arrOddNumbers[i];
+//     }
+// }
+// alert(strModified);
+let arr = [1, 2, 3, 4, 5];
+let func = (item1, item2) => { return item1 + item2; };
+console.log(func(3, 4));
+console.log(
+    arr.reduce(
+        (itemA, itemB) => { return itemA + itemB; },0
+    )
+);
+
+function myConcat(separator) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    return args.join(separator);
+  }
+  console.log(
+       myConcat(', ', 'red', 'orange', 'blue'));
