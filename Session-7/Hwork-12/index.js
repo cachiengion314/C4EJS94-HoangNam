80927be61f9759ba18867a34162029d02977db7d
Hoang Nam <cachiengion314@gmail.com>
2020-09-04 16:04:43 +0700

    Update ss7 part 7

--- Session-7/Hwork-12/index.js
+++ Session-7/Hwork-12/index.js
@@ -1,59 +1,73 @@
 const items = [
     `Backpack`, `MiBand watch`, `Ring`
 ]
+let itemsDivTag = [];
 let allLITag = document.getElementsByTagName(`LI`);
 let ulItems = document.getElementsByTagName(`UL`)[0];
 
-console.log(`${ulItems.tagName}`);
+console.log(`${ulItems.textContent}`);
 
 for (let i = 0; i < allLITag.length; i++) {
-    console.log(allLITag[i]);
+    console.log(allLITag[i].textContent);
+    allLITag[i].remove();
+    i--;
 }
+// Create a remove button function
+function createRemoveButton(callback) {
+    let buttonElement = document.createElement(`button`);
+    let buttonText = document.createTextNode(`remove`);
+    buttonElement.appendChild(buttonText);
+
+    buttonElement.addEventListener(`click`, callback);
+
+    return buttonElement;
+}
+// Create a list of brand and make it the child of div
 for (let i = 0; i < items.length; i++) {
     let liTag = document.createElement(`li`);
-    let text = document.createTextNode(items[i]);
-    liTag.appendChild(text);
+    let liTagText = document.createTextNode(items[i]);
+    liTag.appendChild(liTagText);
 
     let divTag = document.createElement(`div`);
     divTag.appendChild(liTag);
     ulItems.appendChild(divTag);
+
+    itemsDivTag.push(divTag);
+
+    let buttonElement = createRemoveButton(
+        () => {
+            divTag.remove();
+            console.log(itemsDivTag.indexOf(divTag));
+        });
+
+    divTag.appendChild(buttonElement);
 }
-allLITag[0].remove();
-allLITag[1].remove();
-let addBtns = document.getElementsByTagName(`BUTTON`);
+// Get the add button
+let addBtn = document.getElementsByTagName(`BUTTON`)[0];
 let nameInput = document.getElementById(`name_input`);
 
-let addBtn = addBtns[0];
-
-function createButtonElement(idNumber) {
-    let buttonElement = document.createElement(`button`);
-    let text = document.createTextNode(`remove`);
-    buttonElement.appendChild(text);
-    ulItems.appendChild(buttonElement);
-
-    let divTag = document.createElement(`div`);
-    divTag.appendChild(liTag);
-    ulItems.appendChild(divTag);
-
-    buttonElement.addEventListener(`click`,
-        () => {
-            allLITag[idNumber].remove();
-
-        });
-}
-
-function clickCallback() {
+function clickAddCallback() {
     console.log(`Button clicked!!!`);
     console.log(nameInput.value);
     items.push(nameInput.value);
     console.log(items);
+
     let newItemsElement = document.createElement(`LI`);
     let text = document.createTextNode(nameInput.value);
     newItemsElement.appendChild(text);
-    ulItems.appendChild(newItemsElement);
 
-    createButtonElement(items.indexOf(nameInput.value));
+    let divTag = document.createElement(`div`);
+    itemsDivTag.push(divTag);
+    let removeButton = createRemoveButton(
+        () => {
+            divTag.remove();
+            console.log(itemsDivTag.indexOf(divTag));
+        }
+    )
+    divTag.appendChild(newItemsElement);
+    divTag.appendChild(removeButton);
+    ulItems.appendChild(divTag);
 
     nameInput.value = null;
 }
-addBtn.addEventListener(`click`, clickCallback);
+addBtn.addEventListener(`click`, clickAddCallback);
