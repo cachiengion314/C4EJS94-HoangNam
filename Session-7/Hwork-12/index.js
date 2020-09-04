8e239291ab2eaf079a241e5bee211d1f1f08ca7c
Hoang Nam <cachiengion314@gmail.com>
2020-09-04 13:17:58 +0700

    ss7 part 6

--- Session-7/Hwork-12/index.js
+++ Session-7/Hwork-12/index.js
@@ -1,18 +1,59 @@
 const items = [
     `Backpack`, `MiBand watch`, `Ring`
 ]
-let itemsList = document.getElementsByTagName(`LI`);
-let listUl = document.getElementsByTagName(`UL`);
-console.log(`${listUl[0].tagName}`);
-for (let i = 0; i < itemsList.length; i++) {
-    console.log(itemsList[i]);
+let allLITag = document.getElementsByTagName(`LI`);
+let ulItems = document.getElementsByTagName(`UL`)[0];
+
+console.log(`${ulItems.tagName}`);
+
+for (let i = 0; i < allLITag.length; i++) {
+    console.log(allLITag[i]);
 }
 for (let i = 0; i < items.length; i++) {
-    let tag = document.createElement(`li`);
+    let liTag = document.createElement(`li`);
     let text = document.createTextNode(items[i]);
-    tag.appendChild(text);
+    liTag.appendChild(text);
 
-    listUl[0].appendChild(tag);
+    let divTag = document.createElement(`div`);
+    divTag.appendChild(liTag);
+    ulItems.appendChild(divTag);
 }
-itemsList[0].remove();
-itemsList[1].remove();
+allLITag[0].remove();
+allLITag[1].remove();
+let addBtns = document.getElementsByTagName(`BUTTON`);
+let nameInput = document.getElementById(`name_input`);
+
+let addBtn = addBtns[0];
+
+function createButtonElement(idNumber) {
+    let buttonElement = document.createElement(`button`);
+    let text = document.createTextNode(`remove`);
+    buttonElement.appendChild(text);
+    ulItems.appendChild(buttonElement);
+
+    let divTag = document.createElement(`div`);
+    divTag.appendChild(liTag);
+    ulItems.appendChild(divTag);
+
+    buttonElement.addEventListener(`click`,
+        () => {
+            allLITag[idNumber].remove();
+
+        });
+}
+
+function clickCallback() {
+    console.log(`Button clicked!!!`);
+    console.log(nameInput.value);
+    items.push(nameInput.value);
+    console.log(items);
+    let newItemsElement = document.createElement(`LI`);
+    let text = document.createTextNode(nameInput.value);
+    newItemsElement.appendChild(text);
+    ulItems.appendChild(newItemsElement);
+
+    createButtonElement(items.indexOf(nameInput.value));
+
+    nameInput.value = null;
+}
+addBtn.addEventListener(`click`, clickCallback);
