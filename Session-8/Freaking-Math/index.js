39d5d955b851b4fb47e19e126dc76e1b1c1b91ee
Hoang Nam <cachiengion314@gmail.com>
2020-09-11 14:46:02 +0700

    Update ss8

--- Session-8/Freaking-Math/index.js
+++ Session-8/Freaking-Math/index.js
@@ -6,10 +6,33 @@ let noBtn = document.getElementById(`no_button`);
 let correctSound = document.getElementById(`corect_sound`);
 let wrongSound = document.getElementById(`wrong_sound`);
 
-var miliSeconds = 5000;
-var miliSecondIntervalFunc;
+let soundTick = document.getElementById(`setting_tick_sound_tick`);
+let timerTick = document.getElementById(`setting_tick_timer_tick`);
+
+var MAX_TIME = 500;
+var miliSeconds = MAX_TIME;
+var miliSecondIntervalFuncId;
 var randomQuestionIndex, score = 0;
 miliSecondInterval();
+
+function onclickSoundTick() {
+    if (soundTick.textContent == `X`) {
+        soundTick.innerHTML = `&#10003;`;
+    } else {
+        soundTick.textContent = `X`;
+    }
+}
+
+function onclickTimerTick() {
+    if (timerTick.textContent == `X`) {
+        timerTick.innerHTML = `&#10003;`;
+        miliSecondInterval();
+        // continue
+    } else {
+        timerTick.textContent = `X`;
+        // stop
+    }
+}
 
 function getRandomQuestion() {
     randomQuestionIndex = Math.floor(Math.random() * allQuestions.length);
@@ -21,10 +44,15 @@ getRandomQuestion();
 function onclickYesButtonCallback() {
     if (allQuestions[randomQuestionIndex].answer) {
         score++;
-        correctSound.play();
+        if (soundTick.textContent != `X`) {
+            correctSound.play();
+        }
         getRandomQuestion();
     } else {
-        wrongSound.play();
+        if (soundTick.textContent != `X`) {
+            wrongSound.play();
+        }
+        score = 0;
         getRandomQuestion();
     }
 
@@ -37,10 +65,15 @@ yesBtn.addEventListener(`click`, onclickYesButtonCallback);
 function onclickNoButtonCallback() {
     if (!allQuestions[randomQuestionIndex].answer) {
         score++;
-        correctSound.play();
+        if (soundTick.textContent != `X`) {
+            correctSound.play();
+        }
         getRandomQuestion();
     } else {
-        wrongSound.play();
+        if (soundTick.textContent != `X`) {
+            wrongSound.play();
+        }
+        score = 0;
         getRandomQuestion();
     }
 
@@ -51,23 +84,25 @@ noBtn.addEventListener(`click`, onclickNoButtonCallback);
 // count time
 
 function countDownFunc() {
-    if (miliSeconds >= 0) {
+    if (miliSeconds >= 0 && timerTick.textContent != `X`) {
         miliSeconds--;
-        barHead.style.width = `${(miliSeconds/5000 * 100)}%`;
+        barHead.style.width = `${(miliSeconds/MAX_TIME * 100)}%`;
         miliSecondInterval();
-    } else {
+    } else if (miliSeconds < 0 && timerTick.textContent != `X`) {
         resetCountingTime();
+    } else if (timerTick.textContent == `X`) {
+        clearTimeout(miliSecondIntervalFuncId);
     }
 }
 
 function miliSecondInterval() {
-    miliSecondIntervalFunc = setTimeout(countDownFunc, 1);
+    miliSecondIntervalFuncId = setTimeout(countDownFunc, 1);
 }
 
 function resetCountingTime() {
-    clearTimeout(miliSecondIntervalFunc);
-    miliSeconds = 5000;
-    barHead.style.width = `${(miliSeconds/5000 * 100)}%`;
+    clearTimeout(miliSecondIntervalFuncId);
+    miliSeconds = MAX_TIME;
+    barHead.style.width = `${(miliSeconds/MAX_TIME * 100)}%`;
     miliSecondInterval();
     getRandomQuestion();
 }
