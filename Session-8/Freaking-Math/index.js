b0ac2e282122d075ca0ce6e127942718a0feaaa0
Hoang Nam <cachiengion314@gmail.com>
2020-09-11 12:10:41 +0700

    update ss 8 them 2

--- Session-8/Freaking-Math/index.js
+++ Session-8/Freaking-Math/index.js
@@ -2,7 +2,7 @@ let question = document.getElementById(`question`);
 let yesBtn = document.getElementById(`yes_button`);
 let noBtn = document.getElementById(`no_button`);
 
-var randomQuestionIndex = 0;
+var randomQuestionIndex, score = 0;
 
 function getRandomQuestion() {
     randomQuestionIndex = Math.floor(Math.random() * allQuestions.length);
@@ -10,9 +10,27 @@ function getRandomQuestion() {
 }
 getRandomQuestion();
 
-function yesOnclickFunc() {
+function onclickYesButtonCallback() {
     if (allQuestions[randomQuestionIndex].answer) {
-
+        score++;
+        console.log(`score: ${score}`);
+        getRandomQuestion();
+    } else {
+        console.log(`score: ${score}`);
+        getRandomQuestion();
     }
 }
-yesBtn.onclick = yesOnclickFunc;
+
+yesBtn.addEventListener(`click`, onclickYesButtonCallback);
+
+function onclickNoButtonCallback() {
+    if (!allQuestions[randomQuestionIndex].answer) {
+        score++;
+        console.log(`score: ${score}`);
+        getRandomQuestion();
+    } else {
+        console.log(`score: ${score}`);
+        getRandomQuestion();
+    }
+}
+noBtn.addEventListener(`click`, onclickNoButtonCallback);
