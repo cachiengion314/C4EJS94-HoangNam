388ab1125e118724b9439d4f59f0f9b0d6a58334
Hoang Nam <cachiengion314@gmail.com>
2020-09-17 02:17:31 +0700

    Hoan thien bai tap ss9

--- Session-9/Hwork-9/index.js
+++ Session-9/Hwork-9/index.js
@@ -1,62 +1,77 @@
 let h1TitleTag = document.getElementsByTagName(`H1`)[0];
 let ulListTag = document.getElementById(`list_all_songs`)
+let topTypeSelect = document.getElementById("top_type_select");
+let topTypes = `topalbums`;
+let numberOfSongSelect = document.getElementById("number_of_song_select");
+let limitSongs = `limit=10`;
+
+createAllHtmlElement(topTypes, limitSongs);
 
 let albumsSongObject = {
     "Top albums": `topalbums`,
     "Top songs": `topsongs`,
 }
 let numberOrderObject = {
-    "10": `null`,
-    "15": `null`,
-    "20": `null`,
-    "25": `null`,
-    "30": `null`,
-    "50": `null`,
+    "10": `limit=10`,
+    "15": `limit=15`,
+    "20": `limit=20`,
+    "25": `limit=25`,
+    "30": `limit=30`,
+    "50": `limit=50`,
 }
 window.onload = function() {
-    createAllHtmlElement(`topalbums`, `limit=10`);
-
-    var typeSelect = document.getElementById("type_select");
     for (let itemName in albumsSongObject) {
-        typeSelect.options[typeSelect.options.length] = new Option(itemName, itemName);
+        topTypeSelect.options[topTypeSelect.options.length] = new Option(itemName, itemName);
     }
-    typeSelect.onchange = function() {
-        console.log(typeSelect.value);
-        let selectType = albumsSongObject[typeSelect.value];
-        createAllHtmlElement(selectType, `limit=15`);
-    }
-
-    var numberOfSongSelect = document.getElementById("number_of_song_select");
     for (let itemName in numberOrderObject) {
         numberOfSongSelect.options[numberOfSongSelect.options.length] = new Option(itemName, itemName);
     }
+
+    topTypeSelect.onchange = function() {
+        console.log(topTypeSelect.value);
+        topTypes = albumsSongObject[topTypeSelect.value];
+        createAllHtmlElement(topTypes, limitSongs);
+    }
+
     numberOfSongSelect.onchange = function() {
         console.log(numberOfSongSelect.value);
+        limitSongs = numberOrderObject[numberOfSongSelect.value];
+        createAllHtmlElement(topTypes, limitSongs);
     }
 }
 
-function getApi(type = `topsongs`, limit = `limit=15`) {
+function getApi(type = `topalbums`, limit = `limit=10`) {
     return `https://itunes.apple.com/us/rss/${type}/all/${limit}/json`;
 }
 
-function getAllData() {
-    return axios.get(getApi());
+function getAllData(type = `topalbums`, limit = `limit=10`) {
+    return axios.get(getApi(type, limit));
 }
 
-function createAllHtmlElement(type, limit) {
+function createAllHtmlElement(type = `topalbums`, limit = `limit=10`) {
     ulListTag.innerHTML = "";
     getAllData(type, limit).then((rawData) => {
         h1TitleTag.textContent = rawData.data.feed.title.label;
         let songsDetailArr = rawData.data.feed.entry;
+
         for (let i = 0; i < songsDetailArr.length; i++) {
+            let imageUrl = songsDetailArr[i]["im:image"][2].label;
+            let titleName = songsDetailArr[i]["im:name"].label;
+            let artistName = songsDetailArr[i]["im:artist"].label;
+            let previewHref;
+            if (topTypes == `topalbums`) {
+                previewHref = songsDetailArr[i]["link"].attributes.href;
+            } else {
+                previewHref = songsDetailArr[i]["link"][1].attributes.href;
+            }
+
             ulListTag.insertAdjacentHTML(`beforeend`, `
             <li class="one_song">
-            <img src="" alt="">
-            <div>ten bai hat</div>
-            <div>ten ca si</div>
-            <div>Preview</div>
+            <img src="${imageUrl}" alt="">
+            <h4 class="text">${titleName}</h4>
+            <div class="text">${artistName}</div>
+            <a href="${previewHref}" target="_blank">Preview</a>
             </li>`)
         }
-
     });
 }
